4897f7ca84ae7c07925488870a967370c40cb2b7
Christian Martinez <mcchris13e@gmail.com>
2023-10-01 21:03:15 -0500

    Funcionalidad para mostrar los productos al usuario

---
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -0,0 +1,24 @@
+
+package com.ecommerce.controller;
+
+import com.ecommerce.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    
+    @Autowired
+    private ProductoService productoService;
+    
+    @GetMapping("")
+    public String home(Model modelo) {
+        modelo.addAttribute("productos", productoService.listar());
+        return "usuario/home";
+    }
+    
+}
