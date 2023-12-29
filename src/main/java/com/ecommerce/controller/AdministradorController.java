bcde1c8174f5d11c3d3c761fcca073a16fd52bdd
Christian Martinez <mcchris13e@gmail.com>
2023-12-28 19:39:42 -0500

    Se agregó la vista de las ordenes y su detalle

--- src/main/java/com/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/ecommerce/controller/AdministradorController.java
@@ -1,13 +1,18 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.model.Orden;
 import com.ecommerce.model.Producto;
+import com.ecommerce.service.OrdenService;
 import com.ecommerce.service.ProductoService;
 import com.ecommerce.service.UsuarioService;
+import jakarta.servlet.http.HttpSession;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,6 +24,9 @@ public class AdministradorController {
     
     @Autowired
     private UsuarioService usuarioService;
+    
+    @Autowired
+    private OrdenService ordenService;
 
     @GetMapping("")
     public String inicio(Model model) {
@@ -33,5 +41,21 @@ public class AdministradorController {
         var usuarios = usuarioService.findAll();
         modelo.addAttribute("usuarios", usuarios);
         return "administrador/usuarios";
+    }
+    
+    @GetMapping("/ordenes")
+    public String listaCompras(Model modelo) {
+        modelo.addAttribute("ordenes", ordenService.findAll());
+        
+        return "administrador/ordenes";
+    }
+    
+    @GetMapping("/detalle/{id}")
+    public String detalleCompra(@PathVariable Integer id, Model modelo) {
+        
+        Optional<Orden> o = ordenService.buscar(id);
+        modelo.addAttribute("detalles", o.get().getDetalle());
+     
+        return "administrador/detalleorden";
     }
 }
